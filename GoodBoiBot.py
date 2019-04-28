598d4987ba8643f4f426b0cbdf4ed2afd4d72056
George Shao <GeorgeShao123@gmail.com>
2019-04-27 20:45:43 -0400

    Setup

---
+++ GoodBoiBot.py
@@ -0,0 +1,36 @@
+from discord.ext.commands import Bot
+import
+
+BOT_PREFIX = "?"
+file = open("TOKEN.txt","r")
+TOKEN = file.read()
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user.name}")
+
+@client.event
+async def on_message(message):
+    #Prevents bot from responding to other bots, including itself.
+    if message.author.bot:
+        return
+
+    #Checks if sent message starts with "hello" and responds
+    if message.content.lower().startswith("hello"):
+        msg = f"Hello {message.author.mention}"
+        await client.send_message(message.channel, msg)
+
+@client.command()
+#Command for "?ping" that responds with "Pong"
+async def ping():
+    await client.say("Pong")
+
+@client.command()
+#Command for "?square x" where x is a number that responds with the square of x.
+async def square(number: int):
+    squared_value = number ** 2
+    await client.say(f"{number} squared is {squared_value}")
+
+client.run(TOKEN)
