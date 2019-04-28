5ad47da6d5e4eed79aa2e0f12418d9a7aee67e76
George Shao <GeorgeShao123@gmail.com>
2019-04-27 21:29:48 -0400

    Attempted fix.

--- GoodBoiBot.py
+++ GoodBoiBot.py
@@ -1,29 +1,40 @@
 from discord.ext.commands import Bot
+from discord.ext import commands
+from discord import Game
+import discord
 
 BOT_PREFIX = "~"
 file = open("TOKEN.txt","r")
 TOKEN = file.read()
 
-client = Bot(command_prefix=BOT_PREFIX)
+client = commands.Bot(command_prefix = '.')
 
+#Confirms that bot is connected to server
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user.name}")
+    await client.change_presence(game=Game(name="with fellow humans"))
+    print("Logged in as " + client.user.name)
+    servers = list(client.servers)
+    print("Connected on " + str(len(client.servers)) + " servers:")
+    for x in range(len(servers)):
+        print('    ' + servers[x-1].name)
 
 @client.event
 async def on_message(message):
-    #Prevents bot from responding to other bots, including itself.
-    if message.author.bot:
-        return
+    #Checks if message is a command
     await client.process_commands(message)
 
-@client.command(pass_context=True)
-async def ping(ctx):
-    await client.say("pong")
+    #Prevents bot from responding to other bots
+    if message.author == client.user:
+        return
 
-@client.command(pass_context=True)
-async def square(ctx, number: int):
-    answer = number ** 2
-    await client.say(f"{number} squared is {answer}")
+    channel = message.channel
+    username = message.author.name
+    time = str(message.timestamp)
+
+@client.command(name="ping", description="Ping Pong", pass_context=True)
+async def ping(ctx):
+    print(ctx.message.author.name + " has triggered ping command at " + str(ctx.message.timestamp))
+    await client.say('Pong!')
 
 client.run(TOKEN)
