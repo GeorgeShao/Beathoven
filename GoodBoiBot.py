5672f025a9ce0f3a3bb35a910f65f0f570e2ca19
George Shao <GeorgeShao123@gmail.com>
2019-04-27 21:02:55 -0400

    Removed more unnecessary code.

--- GoodBoiBot.py
+++ GoodBoiBot.py
@@ -17,14 +17,12 @@ async def on_message(message):
         return
 
 @client.command()
-#Command for "?ping" that responds with "Pong"
 async def ping():
-    await client.say("Pong")
+    await client.say("pong")
 
 @client.command()
-#Command for "?square x" where x is a number that responds with the square of x.
 async def square(number: int):
-    squared_value = number ** 2
-    await client.say(f"{number} squared is {squared_value}")
+    answer = number ** 2
+    await client.say(f"{number} squared is {answer}")
 
 client.run(TOKEN)
